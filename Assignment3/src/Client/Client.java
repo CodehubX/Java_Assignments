d1ec3ed055fd272fd566ae89584bff317e574958
jm <cincek@swz.cz>
2014-10-28 12:40:20 +0100

    tuesday exercise

--- Assignment3/src/Client/Client.java
+++ Assignment3/src/Client/Client.java
@@ -2,7 +2,9 @@ package Client;
 
 import Server.OurServer;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -13,33 +15,33 @@ import java.util.UUID;
  */
 public class Client {
 
-    public static int counterClients = 0;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Scanner sc = new Scanner(System.in);
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("answers.ser"));
         Socket ss = new Socket("localhost", OurServer.port); // vznikne socket, ktery se pripoji na dany port a host
+
         UUID uniqueKey = UUID.randomUUID();
-        counterClients++;
         System.out.println("Client Connection to Server is OK! " + ss.getInetAddress() + " " + ss.getLocalPort() + "\n" + OurServer.frage);
 
-        Question qs = new Question(uniqueKey); // ulozi UUID a pripravi object pro Q/A
+//        Question qs = new Question(uniqueKey); // ulozi UUID a pripravi object pro Q/A
         System.out.println("\nProvide your opinion or ask server for information.");
 
-        for (; ; ) {
-            sc.reset();
-            System.out.println("Choose from the menu - 1 is for input; 2 is for information");
-            try {
-                int menuChoice = sc.nextInt();
-                if (menuChoice == 1) {
-                    System.out.println("your vote please here");
-                    qs.setDeineAbstimmung(sc.next());  // we use here Scanner for input
-                } else if (menuChoice == 2) {
-                    qs.getDeineAbstimmung(); // return answers
-                }
-            } catch (InputMismatchException e) {
-                System.out.println(e.getMessage() + "only number are allowed");
-                sc.close(); // close scanner
+        sc.reset();
+        System.out.println("Choose from the menu - 1 is for input; 2 is for information");
+        try {
+            int menuChoice = sc.nextInt();
+            if (menuChoice == 1) {
+                oos.writeInt(1);
+                oos.writeObject(uniqueKey);
+                //                    qs.setDeineAbstimmung(sc.next());  // we use here Scanner for input
+            } else if (menuChoice == 2) {
+                oos.writeInt(2);
+                //                    qs.getDeineAbstimmungPerClient(); // return answers
             }
+        } catch (InputMismatchException e) {
+            System.out.println(e.getMessage() + "only number are allowed");
+            sc.close(); // close scanner
         }
+
     }
 }
--- Assignment3/src/Server/ThreadPoolServer.java
+++ Assignment3/src/Server/ThreadPoolServer.java
@@ -1,9 +1,11 @@
 package Server;
 
-import Client.*;
-
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.UUID;
 
 /**
  * Created by jm on 10/25/2014.
@@ -12,34 +14,48 @@ public class ThreadPoolServer implements Runnable {
     ObjectInputStream ois = null;
     ObjectOutputStream ous = null;
     Question qus;
+
     Socket soc;
 
     public ThreadPoolServer(Socket socket) {
         try {
             this.soc = socket;
-            qus = new Question();
             ois = new ObjectInputStream(new FileInputStream("answers.ser"));
-            ous = new ObjectOutputStream(new FileOutputStream("answers.ser"));
+            //            ois = new ObjectInputStream(soc.getInputStream());
+
+            //            ous = new ObjectOutputStream(new FileOutputStream("answers.ser"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     @Override public void run() {
-        if (Client.counterClients == 0) {
-            try {
-                System.out.println(qus.getDeineAbstimmung());
-                System.out.println("Client :" + /*qus.getId() +*/ " said " + ois.readUTF() + ois.readObject());
-            } catch (EOFException e) {
-                e.getMessage();
-            } catch (IOException e) {
-                e.getLocalizedMessage();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
+        try {
+            System.out.println(ois.readInt());
+            if (ois.readInt() == 1) {
+                System.out.println("your vote please here");
+                UUID uuid = (UUID) ois.readObject();
+                System.out.println(uuid);
+                qus = new Question(uuid);
+                qus.setDeineAbstimmung(ois.readUTF());
+            } else if (ois.readInt() == 2) {
+                qus.getDeineAbstimmungPerClient();
             }
-        } else {
-            System.out.println("There are not clients at the time");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
+        //            try {
+        //                System.out.println("Client :" + /*qus.getId() +*/ " said " + ois.readUTF() + ois.readObject());
+        //            } catch (IOException e) {
+        //
+        //            } catch (ClassNotFoundException e) {
+        //            }
 
+        //        } else {
+        //            System.out.println("There are not clients at the time");
+        //        }
     }
 }
+
